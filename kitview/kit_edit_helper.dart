d8dfcfb22b28fdd2297220e6e64b37e5c43f6527
GangJust <201741884@qq.com>
2022-12-07 10:26:46 +0800

    Update kit_edit_helper.dart

--- kitview/kit_edit_helper.dart
+++ kitview/kit_edit_helper.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 
 /// 默认 [InputDecoration]
-InputDecoration kitInputDecoration({int? maxLines}) {
+InputDecoration kitInputDecoration({int? maxLines, double height = 40.0}) {
   return InputDecoration(
     contentPadding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0),
     border: const OutlineInputBorder(borderSide: BorderSide.none),
     focusedBorder: const OutlineInputBorder(borderSide: BorderSide.none),
     alignLabelWithHint: true,
     constraints: BoxConstraints(
-      maxHeight: maxLines == null || maxLines == 1 ? 38.0 : double.infinity,
-      minHeight: 38.0,
+      maxHeight: maxLines == null || maxLines == 1 ? height : double.infinity,
+      minHeight: height,
     ),
   );
 }
